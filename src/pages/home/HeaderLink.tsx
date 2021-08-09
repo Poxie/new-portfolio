14673150d373d3b93f0242c40817adf0541666eb
Poxie <poxen12@hotmail.com>
2021-08-09 02:25:29 +0200

    Added rel attribute to external links

--- src/pages/home/HeaderLink.tsx
+++ src/pages/home/HeaderLink.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const HeaderLink: React.FC<Props> = ({ children, link, tooltip }) => {
     return(
         <Tooltip className="header-link" tooltip={tooltip}>
-            <a href={link} target="_blank">
+            <a href={link} target="_blank" rel="noreferrer">
                 {children}
             </a>
         </Tooltip>
